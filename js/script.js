2981d2aa61201b97fbf183f8608d72719abd5db1
junasatrio <junsatsatprakoso@gmail.com>
2023-09-27 15:55:41 +0700

    perubahan web

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function getData() {
+    // data collection
+
+    const name=document.getElementById("name").value
+    const email=document.getElementById("email").value
+    const phone=document.getElementById("phone").value
+    const subject=document.getElementById("subject").value
+    const message=document.getElementById("message").value
+
+    //data validation
+    if(name == ""){
+        return alert("Tidak Boleh Kosong")
+    } else if(email == ""){
+        return alert("Tidak Boleh Kosong")
+    } else if(phone == ""){
+        return alert("Tidak Boleh Kosong")
+    } else if(subject == ""){
+        return alert("Tidak Boleh Kosong")
+    } else if(message == ""){
+        return alert("Tidak Boleh Kosong")
+    }
+
+    //execute to email
+    const myEmail = "hi.junsatsatprakoso@gmail.com"
+    let a = document.createElement("a")
+    a.href = `mailto:${myEmail}?subject=${subject}&body= hi my name is ${name} , whay do you call me ${phone} untuk membahaya soal project bisnis kita ${message}`
+    a.click()
+
+    let data ={
+        name,
+        email,
+        phone,
+        subject,
+        message
+    }
+    console.log(data)
+}
